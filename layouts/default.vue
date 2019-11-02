3aa131221b4d4bb5facc5dfff2b105c9c667b3d2
AlenaNik <alena.nik.digital@gmail.com>
2019-11-02 22:50:56 +0100

    added a stucture

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Nav/>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import Nav from './partials/nav';
+export default {
+  components: {
+    Nav
+  }
+}
+</script>
+<style>
+  body {
+    box-sizing: border-box;
+    height: 100%;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+
+
+</style>
---
+++ layouts/partials/nav.vue
@@ -0,0 +1,36 @@
+<template>
+    <header>
+      <nav class="navbar nav w-16 fixed min-h-screen flex justify-center items-center">
+        <ul>
+          <li
+            class="mb-10 ml-6"
+          ><nuxt-link
+            to="/">Home</nuxt-link></li>
+          <li
+            class="mb-10  ml-6"
+          ><nuxt-link
+            to="/showcase">Work</nuxt-link></li>
+          <li
+            class="mb-10  ml-6"
+          ><nuxt-link
+            to="/contact">Contact</nuxt-link></li>
+        </ul>
+      </nav>
+    </header>
+</template>
+
+<script>
+  export default {
+    name: 'Nav'
+  }
+</script>
+
+<style>
+.navbar {
+  color: black;
+  font-family: "Helvetica Neue";
+  font-size: 16px;
+  font-weight: 700;
+}
+
+</style>
